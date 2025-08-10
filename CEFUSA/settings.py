7c4cf16f71cb09167cd1dc666fc971610b5e0fbb
Salazar1022 <ssalazarh3@eafit.edu.co>
2025-08-10 13:36:59 -0500

    Change DB name

--- CEFUSA/settings.py
+++ CEFUSA/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'CEFUSA.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'CEFUSA.db',
     }
 }
 
