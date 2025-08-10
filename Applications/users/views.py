8e4ba4936502b3cd201fd0dc5c1840c6c279adaa
Salazar1022 <ssalazarh3@eafit.edu.co>
2025-08-10 13:22:40 -0500

    Add html views and organize the project folders

--- Applications/users/views.py
+++ Applications/users/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def usersManagement(request):
-    return render(request, 'usersManagement.html')
+    return render(request, 'index.html')
