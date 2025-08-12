aad21e2f97e5253a5388e3d5ac20a5b41d6536cf
Salazar1022 <ssalazarh3@eafit.edu.co>
2025-08-12 16:20:02 -0500

    fix some errors in create feature

--- Applications/users/forms.py
+++ Applications/users/forms.py
@@ -7,7 +7,27 @@ class AcudienteForm(forms.ModelForm):
         model = Acudiente
         fields = '__all__'
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field.widget.__class__.__name__ in ['Select', 'SelectMultiple']:
+                field.widget.attrs['class'] = 'form-select'
+            elif field.widget.__class__.__name__ in ['CheckboxInput', 'RadioSelect']:
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
 class JugadorForm(forms.ModelForm):
     class Meta:
         model = Jugador
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field.widget.__class__.__name__ in ['Select', 'SelectMultiple']:
+                field.widget.attrs['class'] = 'form-select'
+            elif field.widget.__class__.__name__ in ['CheckboxInput', 'RadioSelect']:
+                field.widget.attrs['class'] = 'form-check-input'
+            else:
+                field.widget.attrs['class'] = 'form-control'
