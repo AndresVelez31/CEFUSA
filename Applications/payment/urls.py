4692a3bd8852bf1f0c6e7c75ca315791005793cb
Sara Hurtado <shurtadom3@eafit.edu.co>
2025-09-09 20:13:44 -0500

    modify views in payment

--- Applications/payment/urls.py
+++ Applications/payment/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.display_payment, name='display_payment'),
     path('pagos/crear/', views.create_payment, name='create_payment'),
-    path('pagos/gestionar/', views.display_payment, name='display_payment'),
+    path('pagos/gestionar/', views.display_payment, name='payment_management'),
     path('pagos/<int:payment_id>/details/', views.get_payment_details, name='get_payment_details'),
     path('pagos/<int:payment_id>/edit/', views.get_payment_edit_form, name='get_payment_edit_form'),
     path('pagos/<int:payment_id>/update/', views.update_payment, name='update_payment'),
